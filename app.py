bcaf2cfb07c7ed94de5d026aab2f73ef9a7d19a6
GuilhermeShun <akashi.guilherme@aluno.ifsp.edu.br>
2025-07-13 19:39:01 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, request, redirect, url_for, make_response
-
+cores = {'escuro': {'Fundo': '#000000', 'Texto': '#ffffff'}, 
+         'claro': {'Fundo': '#ffffff', 'Texto': '#000000'}}
 app = Flask(__name__)
 app.secret_key = 'abc'
 USUARIO_CADASTRADO = "admin"
@@ -17,83 +18,79 @@ def login():
             # make_response permite que se tenha um maior controle sobre o que será enviado para o navegador
             # o make_response também permite que sejam enviados arquivos separados para o navegador
             resposta = make_response(redirect(url_for('bemvindo')))
-            resposta.set_cookie('username', usuario, max_age=60*10)
+            resposta.set_cookie('username', usuario)
             session['contador'] = 0
             return resposta
         else:
             mensagem = 'Usuário ou senha inválidos. Tente novamente'
         
-    return render_template('login.html', error=mensagem)
+    return render_template('login.html', error=mensagem, tema=request.cookies.get('tema', 'claro'), cores=cores)
 
 @app.route('/bemvindo', methods=['GET', 'POST'])
-def bemvindo():          
+def bemvindo():
     #retoma o valor definido em resposta.set_cookie('username', usuario, max_age=60*10)
     username = request.cookies.get('username') 
     if not username:
         return redirect(url_for('login'))
-    cores = {'escuro': {'Fundo': '#000000', 'Texto': '#ffffff'}, 
-             'claro': {'Fundo': '#ffffff', 'Texto': '#000000'}}
+    
     if request.method == 'POST':
         resposta = make_response(redirect(url_for('bemvindo')))
-        tema = request.form['tema']
-        resposta.set_cookie('tema', tema, max_age=60*30)
+        if 'tema' in request.form:
+            tema = request.form['tema']
+            resposta.set_cookie('tema', tema, max_age=60*30)
+        elif 'preferencia' in request.form:
+            preferencia = request.form['preferencia']
+            resposta.set_cookie('preferencia', preferencia)
         return resposta
+                        
     else:
         session['contador'] += 1
 
-    return render_template('bemvindo.html', contador=session['contador'], user=username, tema=request.cookies.get('tema', 'claro'), cores=cores)
+    return render_template('bemvindo.html', contador=session['contador'], user=username, tema=request.cookies.get('tema', 'claro'), preferencia=request.cookies.get('preferencia', 'nenhum'), cores=cores)
         
-@app.route('/tema', methods=['GET', 'POST'])
-def tema():
-    if request.method == 'POST':
-        cor = request.form['cor']
-        resposta = make_response(redirect(url_for('bemvindo')))
-        resposta.set_cookie('cor', cor, max_age=60*10)
-        return resposta
-    return render_template('tema.html')
-
 @app.route('/logout')
 def logout():
     resposta = make_response(redirect(url_for('login')))
     resposta.set_cookie('username', '', expires=0)
     # expires=0 faz com que o cookie deixe de existir
+    
     return resposta
 
 @app.route('/bemvindo/noticia1')
 def exibir_noticia1():
-    return render_template("/noticias/noticia1.html")
+    return render_template("/noticias/noticia1.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 @app.route('/bemvindo/noticia2')
 def exibir_noticia2():
-    return render_template("/noticias/noticia2.html")
+    return render_template("/noticias/noticia2.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 @app.route('/bemvindo/noticia3')
 def exibir_noticia3():
-    return render_template("/noticias/noticia3.html")
+    return render_template("/noticias/noticia3.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 @app.route('/bemvindo/noticia4')
 def exibir_noticia4():
-    return render_template("/noticias/noticia4.html")
+    return render_template("/noticias/noticia4.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 @app.route('/bemvindo/noticia5')
 def exibir_noticia5():
-    return render_template("/noticias/noticia5.html")
+    return render_template("/noticias/noticia5.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 @app.route('/bemvindo/noticia6')
 def exibir_noticia6():
-    return render_template("/noticias/noticia6.html")
+    return render_template("/noticias/noticia6.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 @app.route('/bemvindo/noticia7')
 def exibir_noticia7():
-    return render_template("/noticias/noticia7.html")
+    return render_template("/noticias/noticia7.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 @app.route('/bemvindo/noticia8')
 def exibir_noticia8():
-    return render_template("/noticias/noticia8.html")
+    return render_template("/noticias/noticia8.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 @app.route('/bemvindo/noticia9')
 def exibir_noticia9():
-    return render_template("/noticias/noticia9.html")
+    return render_template("/noticias/noticia9.html", cores=cores, tema=request.cookies.get('tema', 'claro'))
 
 if __name__ == '__main__':
     app.run(debug=True)
